a4159ce02bf76a64b6cd44b1d5f0e24e3227d4d5
Jinlong Liu <nnuljl@gmail.com>
2018-01-31 15:18:29 +0800

    prepare for output with json file

--- VehicleProject/Program.cs
+++ VehicleProject/Program.cs
@@ -14,6 +14,7 @@
 #endregion
 
 using System;
+using System.IO;
 
 namespace VehicleProject
 {
@@ -24,33 +25,6 @@ namespace VehicleProject
             //Print car1 information
             Car car1 = new Car("NF123456", 147, 200, "green");
             car1.Print();
-
-            ///Print car2 information
-            Car car2 = new Car("NF654321", 150, 195, "blue");
-            car2.Print();
-            Console.WriteLine();
-
-            ///Compare car1 and car2 
-            if (car1.Equals(car2))
-            {
-                Console.WriteLine("Car1 is the same as Car2.");
-            }
-            else
-            {
-                Console.WriteLine("Car1 is different from Car2.");
-            }
-
-            ///Print plane information
-            Plane plane = new Plane("LN1234", 1000, 30, 2, 2);
-            plane.Print();
-            Console.WriteLine();
-
-            plane.Move();   //ask plane to fly
-            car1.Move();    //ask car1 to driver
-            
-            ///Print boat information
-            Boat boat = new Boat("ABC123", 100, 30, 500);
-            boat.Print();
 
             ///The program wait for stopping
             Console.ReadLine();
